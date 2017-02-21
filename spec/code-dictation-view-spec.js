bb276daf48696ec4442a466024c474a0d95689f1
Matthew Dahl <dahl.matthew@gmail.com>
2017-02-21 09:17:21 -0800

    Initial commit

---
+++ spec/code-dictation-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodeDictationView from '../lib/code-dictation-view';
+
+describe('CodeDictationView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
