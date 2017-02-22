e164ca3d5a6649066254d77d5065df571c6ec62c
Matthew Dahl <dahl.matthew@gmail.com>
2017-02-21 17:06:04 -0800

    Start over and better structure code

--- spec/code-dictation-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CodeDictationView from '../lib/code-dictation-view';
-
-describe('CodeDictationView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
